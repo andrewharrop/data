1fc4f5b6fdce27d2153e6a4c615680085d055567
andrewharrop <60928763+andrewharrop@users.noreply.github.com>
2021-02-04 12:57:40 -0500

    initial commit

---
+++ united_states/data_gov/nyc_crimes.py
@@ -0,0 +1,11 @@
+import requests
+import time
+import datetime
+import json
+
+base_url = "https://data.ny.gov/api/views/ptx6-hh79/rows.json?accessType=DOWNLOAD"
+
+def format_file():
+    json_file = json.loads(request.get(base_url).contend.decode())
+
+print(datetime.datetime.fromtimestamp(1366216625).strftime('%Y-%m-%d %H:%M:%S'))
