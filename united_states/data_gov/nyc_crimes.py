b70bfee0cf7583a381193eb6b88b0ced1efe4675
andrewharrop <60928763+andrewharrop@users.noreply.github.com>
2021-02-04 13:05:56 -0500

    Test commit

--- united_states/data_gov/nyc_crimes.py
+++ united_states/data_gov/nyc_crimes.py
@@ -7,5 +7,5 @@ base_url = "https://data.ny.gov/api/views/ptx6-hh79/rows.json?accessType=DOWNLOA
 
 def format_file():
     json_file = json.loads(request.get(base_url).contend.decode())
-
+    print(json_file)
 print(datetime.datetime.fromtimestamp(1366216625).strftime('%Y-%m-%d %H:%M:%S'))
